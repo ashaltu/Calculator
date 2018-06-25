3590a225dc4a71b762d2966ab2ad0bdc114d9d4f
Abdul Shaltu <abdul.shaltu17@gmail.com>
2018-06-24 20:37:10 -0700

    Revamp

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,60 +8,56 @@ namespace Calculator
         {
             bool running = true;
             Console.WriteLine("1)Add\n2)Subtract\n3)Multiply\n4)Divide");
-            while(running){
+            while (running)
+            {
+                int num1, num2;
                 string choice = Console.ReadLine();
-                if(choice == ("add") || choice == "1"){
-                    Console.WriteLine("Answer: "+add());
-                }else if(choice==("subtract")|| choice == "2"){
-                    Console.WriteLine("Answer: " +subtract());
-                }else if (choice==("multiply")|| choice == "3"){
-                    Console.WriteLine("Answer: " +multiply());
-                }else if (choice==("divide")|| choice == "4"){
-                    Console.WriteLine("Answer: " +divide().ToString("#.###"));
-                }else{
-                    Console.WriteLine("INVALID CHOICE!");
+                switch (choice)
+                {
+                    case "add":
+                    case "1":
+                        init(out num1, out num2);
+                        Console.WriteLine("Answer: " + (num1 + num2));
+                        break;
+                    case "subtract":
+                    case "2":
+                        init(out num1, out num2);
+                        Console.WriteLine("Answer: " + (num1 + num2));
+                        break;
+                    case "multiply":
+                    case "3":
+                        init(out num1, out num2);
+                        Console.WriteLine("Answer: " + (num1 + num2));
+                        break;
+                    case "divide":
+                    case "4":
+                        init(out num1, out num2);
+                        Console.WriteLine("Answer: " + (num1 + num2));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        continue;
                 }
 
                 Console.WriteLine("Perform another operation y/n?");
                 choice = Console.ReadLine();
-                if(choice == "y" || choice == "yes" || choice == "yea" || choice == "yeah"){
+                if (choice == "y" || choice == "yes" || choice == "yea" || choice == "yeah")
+                {
                     running = true;
-                }else{
+                }
+                else
+                {
                     running = false;
                 }
             }
         }
 
-        public static int add(){
-            Console.Write("Enter first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            return num1 + num2;
-        }
-        public static int subtract()
+        public static void init(out int num1, out int num2)
         {
             Console.Write("Enter first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            return num1 - num2;
-        }
-        public static int multiply()
-        {
-            Console.Write("Enter first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            return num1 * num2;
-        }
-        public static double divide()
-        {
-            Console.Write("Enter first number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter second number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            return (num1 / num2);
+            num2 = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
